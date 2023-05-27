b3ced15732a87b03087ce546cdc973f30f38241c
hilary433 <honorableisrael>
2023-05-27 06:30:04 +0100

    updated New logical assignment operators in JavaScript

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ c ??= "default value"; // c is now "default value"
 // These operators can be useful for assigning default values to variables,
 //  or for performing other types of operations that are commonly used in conditional statements.
 //  They can help make your code more concise and readable by combining multiple operations into a single statement.
+const isEven = (number) => number % 2 === 0;
+const numbers = [1, 2, 3, 4];
+
+// from first to the last lookup
+console.log(numbers.find(isEven));
+// 2
+console.log(numbers.findIndex(isEven));
+// 1
+
+// from last to the first lookup
+// console.log(numbers.findLast(isEven)); // proposed solution not in use yet for now
+// 4
+// console.log(numbers.findLastIndex(isEven)); // proposed solution not in use yet for now
+// 3
